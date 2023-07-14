379103fa919b340659d7d51d2d49285e44f2e1b4
Lamba237 <simontanyi@gmail.com>
2023-07-14 10:24:20 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - The program prints a character followed
+ *return : Returning 0 to close the program
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
