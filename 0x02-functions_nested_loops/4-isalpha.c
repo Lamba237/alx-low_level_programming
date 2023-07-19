aaa8c93515022dd0d497ab10adcf7c265deca6e3
Simon Eno Tanyi <simontanyi2004@gmail.com>
2023-07-19 10:30:45 +0100

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
- * _isalpha - returns 1 if input is c
- * letter another cases, show 0
- *
- *@c: The ASCII format
- *
-Return: 1 for letters. 0 if otherwise.
+* _isalpha - Shows 1 if the input is c
+* letter Another cases, shows 0
+*
+* @c: The character in ASCII code
+*
+* Return: 1 for letters. Returns 0 otherwise
 */
 int _isalpha(int c)
 {
