877f805b3bd6bcc637fe42d09096dcdab261ee7a
Simon Eno Tanyi <simontanyi2004@gmail.com>
2023-07-19 10:28:33 +0100

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,15 +8,15 @@
  *
 Return: 1 for letters. 0 if otherwise.
 */
-
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90)) 
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
-		return (1);
+	return (1);
 	}
 	else
 	{
-		return (0);
+	return (0);
 	}
+	_putchar('\n');
 }
