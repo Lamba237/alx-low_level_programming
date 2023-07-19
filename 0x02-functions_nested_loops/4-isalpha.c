67a9f32988008b0e0dd41402740f06b1f02bbbb9
Simon Eno Tanyi <simontanyi2004@gmail.com>
2023-07-19 10:07:19 +0100

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - returns 1 if a letter is lower case or upper case
+ *
+ *@c: The ASCII format
+ *
+Return: 1 for upper or lowercase letter. 0 if otherwise.
+*/
+
+int _isalpha(int c)
+{
+	if ( c >= 97 || c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
