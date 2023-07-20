8ecbf2d7ee5d5cb43fa3c18049d18489cf4c3553
Simon Eno Tanyi <simontanyi2004@gmail.com>
2023-07-20 22:44:19 +0100

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *  mul - multiplies two integers.
+ *  @a: first interger
+ *  @b: second interger
+ *  Return: 0
+ */
+int mul(int a, int b)
+{
+	_putchar(a *b);
+	return (0);
+}
