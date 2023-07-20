21b51560c78b0177188c2e584f755909e1b04310
Simon Eno Tanyi <simontanyi2004@gmail.com>
2023-07-20 20:38:35 +0100

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,7 +10,7 @@
 * Return: void
 */
 
-void print_remaining_days(int month, int day, int year)
+void print_remaining_days(int day, int month, int year)
 {
 	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
@@ -26,7 +26,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 	if (month <= 2 && day <= 60)
 		{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		printf("Invalid date: %02d/%02d/%04d\n", day - 31, month, year);
 		}
 	else
 		{
