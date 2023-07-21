feb751209fa06b814ba7c07c5b1d01f67ea3a8a5
Simon Eno Tanyi <simontanyi2004@gmail.com>
2023-07-21 17:00:04 +0100

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,31 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * main - entry block
- * @void: no argument
- * Return: 0 success
- */
+* main - entry block
+* @void: no argument
+* Return: 0 Success
+*/
 int main(void)
 {
 	int i;
 
 	for (i = 1; i < 100; i++)
-		if (i % 3 == 0)
+		if (i % 15 == 0)
 		{
-			printf("Fizz ");
+			printf("FizzBuzz ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		else if(i % 15 == 0)
+		else if (i % 3 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("Fizz ");
 		}
 		else
 		{
 			printf("%d ", i);
 		}
-	printf("Buzz ");
+	printf("Buzz");
 	printf("\n");
 	return (0);
 }
