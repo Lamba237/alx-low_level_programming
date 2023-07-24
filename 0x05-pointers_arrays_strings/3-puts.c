9dd043e3b86dec79b16178624b617ad0fc3d3d03
Simon Eno Tanyi <simontanyi2004@gmail.com>
2023-07-25 00:53:40 +0100

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string.
+ * @str: string.
+ */
+void _puts(char *str)
+{
+	str = " I do not fear computers. I fear the lack of them \n";
+	_putchar(str)'
+}
