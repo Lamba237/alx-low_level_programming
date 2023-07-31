8a5e5ae1f2dfaa3957d6a4b173fc57aeca380879
Simon Eno Tanyi <simontanyi2004@gmail.com>
2023-07-31 14:49:57 +0100

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strspn - length of string.
+ * @s: char pointer.
+ * @accept: string value.
+ *
+ * Return: number of bytes in initial segment.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	for (accept = 0; s[accept] != '\0'; accept++)
+	{
+		return (sizeof(s[accept]);
+				}
+				}
