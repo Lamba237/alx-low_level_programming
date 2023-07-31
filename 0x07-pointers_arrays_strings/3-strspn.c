bcac2fc8ede905938997d010d16b99afb55398db
Simon Eno Tanyi <simontanyi2004@gmail.com>
2023-07-31 20:28:02 +0100

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,8 +8,19 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	for (accept = 0; s[accept] != '\0'; accept++)
+	int i, j;
+	unsigned int length;
+
+	length = 0;
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		return (sizeof(s[accept]);
-				}
-				}
+		for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
+		{
+			if (s[i] == accept[j])
+				length++;
+			if(accept[j] == '\0')
+				return (length);
+		}
+	}
+	return (length);
+}
