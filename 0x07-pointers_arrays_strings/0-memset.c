38aa05bdbe2d018ee4840ea27551a1e8df98fb97
Simon Eno Tanyi <simontanyi2004@gmail.com>
2023-07-31 12:46:45 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,15 +2,15 @@
 /**
  * _memset - Write a function that fills,
  * memory with a constant byte.
- *
- * @s: input value.
+ *@s: input value.
  * @b: input value.
  * @n: input value.
+ *
  * Return: return s,
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	int i = 0;
 
 	while (n > 0)
 	{
