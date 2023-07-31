5c493ce1f96b3d148ef6a342e77f4011adc7d52c
Simon Eno Tanyi <simontanyi2004@gmail.com>
2023-07-31 13:39:24 +0100

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - locaates a character in a string.
+ * @s: pointer string.
+ * @c: character variable.
+ *
+ * Return: c or null.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
