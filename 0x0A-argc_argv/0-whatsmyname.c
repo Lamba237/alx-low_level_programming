5da58ccf4995e3acb5f2a02e9327000da028dded
Lamba237 <simontanyi2004@gmail.com>
2023-08-08 06:37:43 -0700

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
+/**
+ *@argc: integer.
+ *@argv: pointer array.
+ *Return: returns array of pointers
+**/
 int main (int argc, char *argv[])
 {
-  int count;
-
-  printf ("This program was called with \"%s\".\n",argv[0]);
-
-  if (argc > 1)
-    {
-      for (count = 1; count < argc; count++)
-	{
-	  printf("argv[%d] = %s\n", count, argv[count]);
-	}
-    }
-  else
-    {
-      printf("The command had no other arguments.\n");
-    }
-
-  return 0;
+	argv = argv;
+  printf("%d\n", argc - 1);
+  return (0);
 }
