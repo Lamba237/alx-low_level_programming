5142c4c36c4f5592982878520690aea2b4679024
Lamba237 <simontanyi2004@gmail.com>
2023-08-08 06:56:22 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,10 @@
  *@argc: interger value.
  *@argv: array of character pointer.
  *Return: prints number of arguments passed into it.
-**/
+*/
 int main (int argc, char *argv[])
 {
 	argv = argv;
-
 	printf("%d\n",argc - 1);
 	return (0);
 }
