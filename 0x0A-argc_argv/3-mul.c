e9e8c2be29e6604ad1a6545efa7ae1abc476e0a0
Lamba237 <simontanyi2004@gmail.com>
2023-08-09 13:51:49 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,20 +2,18 @@
 #include <stdlib.h>
 /**
  *main - program to print two numbers.
- *
- *Description: The program returns 0.
  *@argc: argument count.
  *@argv: argument vector.
- *
- *Returns: returns 1 if argument < 1 and 0 if arg > 1.
- */
+ *Return: returns 1 if argument < 1 and 0 if arg > 1.
+**/
 int main(int argc, char *argv[])
 {
 	int sum;
+	/*atoi converts a string to an interger*/
 	int a = atoi(argv[1]);
 	int b = atoi(argv[2]);
 
-	if (argc < 1)
+	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (1);
