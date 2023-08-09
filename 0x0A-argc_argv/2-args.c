3f528bb163d471a5e8ceb08e38dc7f03fc16e064
Lamba237 <simontanyi2004@gmail.com>
2023-08-09 07:08:07 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,11 +6,11 @@
  *
  *Return: 0 or 1
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	while (argc--)
 	{
-		printf("%s\n",*argv++);
+		printf("%s\n", *argv++);
 	}
 	return (0);
 }
