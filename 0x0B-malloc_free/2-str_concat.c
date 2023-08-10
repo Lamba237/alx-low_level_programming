c35e7fff3b1a522a943ca92f740b98cf4e6d8c58
Lamba237 <simontanyi2004@gmail.com>
2023-08-10 09:34:10 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *str_concat - concantenates two strings.
+ *@s1: input string.
+ *@s2: input string.
+ *Return: pointer to new space in memory.
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = j = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	str = malloc(sizeof(char) * (i + j + 1));
+	if (str == NULL)
+		return (NULL);
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[j] = s2[j];
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
