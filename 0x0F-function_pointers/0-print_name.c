34096df2d8dc0afe6a829a2542f5731d167fbcbe
Lamba237 <simontanyi2004@gmail.com>
2023-08-16 17:55:54 -0700

    bug_fixed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,10 @@
 #include "function_pointers.h"
-
 /**
  *print_name - prints a name.
  *@name: first element
- *@f: function pointer.
- *
- *Description: Nothing else to describe.
- *Return: 0 as usual.
+ *@f: function pointer
+ *Description: Nothing else to describe
 */
-
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
