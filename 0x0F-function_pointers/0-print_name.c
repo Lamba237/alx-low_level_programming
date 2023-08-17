5e3460e9d037eb0f2f93d4cf2b08fc148448dd0a
Lamba237 <simontanyi2004@gmail.com>
2023-08-16 18:18:33 -0700

    up to date

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  *print_name - prints a name.
  *@name: first element
@@ -7,5 +8,6 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && *name != '\0' && f != NULL)
+		f(name);
 }
