edf85d86eacb331a21a80e0749ff9fde2fd6eada
Simon tanyi <simontanyi2004@gmail.com>
2023-10-01 13:09:47 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - prints a string in reverse.
+ *@s: string to be printed
+ *
+ *Return: returns a string in reverse order or null
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
