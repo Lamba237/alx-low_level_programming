e51b862f1bf83d034f8d159c74b9b90c409e0d1c
Simon tanyi <simontanyi2004@gmail.com>
2023-10-01 11:50:48 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		s++;
-		_puts_recursion(s);
 		_putchar('\n');
+		return;
 	}
-	return;
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
