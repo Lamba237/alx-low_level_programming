6021621d40a7230bdc20e9fdf4d3d49532fe2ecb
Simon tanyi <simontanyi2004@gmail.com>
2023-10-04 22:02:36 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,23 @@
  */
 int main(int argc, char *argv[])
 {
-	int result;
 
-	if (argc < 2 && argc != 3)
+	int result;
+	int num1;
+	int num2;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	result = atoi(argv[1]) * atoi(argv[2]);
+	else
+	{
+	result = num1 * num2;
 	printf("%d\n", result);
+	}
 	return (0);
 }
