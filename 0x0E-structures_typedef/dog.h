8c38722b2bf577bfada883c260877cbb14fb7ec3
Simon tanyi <simontanyi2004@gmail.com>
2023-10-10 20:56:31 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - header file that contains
+ * information about an owner and his/her dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owners name;
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
