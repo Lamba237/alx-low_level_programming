d9b82330520e09bd50e2f6f5f063951107bc0b18
Simon tanyi <simontanyi2004@gmail.com>
2023-10-12 05:38:58 -0700

    prints_an_array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ *array_iterator - prints an array of int
+ *@array: array to be printed
+ *@size: size of array
+ *@action: pointer to function
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
