7d932166f5da273d038518f6a3ca6739ba304fb2
Simon tanyi <simontanyi2004@gmail.com>
2023-10-23 09:36:18 -0700

    light updates on header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,12 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stddef.h>
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -13,5 +19,18 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 int _putchar(char c);
+
+/**
+ * struct listint_s - structure node
+ * @str: string to be passed
+ * @len: length of list
+ * @next: pointer to next node
+ */
+typedef struct listint_s
+{
+	char *str;
+	unsigned int len;
+	struct listint_s *next;
+} listint_t;
 
 #endif
