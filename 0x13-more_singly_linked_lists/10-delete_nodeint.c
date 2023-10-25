d373d73b8760d8318fea2b951090d8152b789c5b
Simon tanyi <simontanyi2004@gmail.com>
2023-10-25 15:47:46 -0700

    updates

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	for (n = 0; n < index - 1 && delete != NULL; n++)
 	{
 		delete = (*head);
-		*head = (*head)->next;
+		delete = delete->next;
 	}
 	free(delete);
 	return (1);
