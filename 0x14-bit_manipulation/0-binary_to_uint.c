cfdc81658641168a89077cbf2ee4abc5934b61b5
Simon tanyi <simontanyi2004@gmail.com>
2023-10-26 07:26:50 -0700

    function that converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * binary_to_uint - a function that converts a binary
+ * number to an unsigned int
+ * @b: binary number to be converted
+ * Return: unsigned int value after convention
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int interger = 0;
+	int n;
+
+	if (b == NULL || (b[interger] != '0' && b[interger] != '1'))
+	{
+			return (0);
+	}
+
+	for (n = strlen(b) - 1; n >= 0; n--)
+	{
+		interger += (b[n] - '0') * _pow_recursion(2, strlen(b) - n - 1);
+	}
+	return (interger);
+}
+/**
+ *_pow_recursion - returns the value of x raised to the power of y
+ *@x: first interger
+ *@y: second interger
+ *
+ *Return: return x raised to the power of y else return -1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
