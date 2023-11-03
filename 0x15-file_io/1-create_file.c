6b4638043b9be5b6ea303876a7f0047d1cd564a3
Simon tanyi <simontanyi2004@gmail.com>
2023-11-03 02:27:18 -0700

    updates on file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,6 +32,10 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
+	if (access(filename, F_OK) == 0)
+	{
+		fd = open(filename, O_TRUNC);
+	}
 	close(fd);
 	return (1);
 }
