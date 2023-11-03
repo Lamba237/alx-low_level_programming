bd18fe2f80c1694b0ede574c958113993ec8b140
Simon tanyi <simontanyi2004@gmail.com>
2023-11-03 05:27:54 -0700

    updates

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	/* writing into the file using */
-	n_write = write(fd, text_content, strlen(text_content));
+	n_write = write(fd, text_content, sizeof(text_content));
 	if (n_write == -1)
 	{
 		write(1, "fails", 6);
