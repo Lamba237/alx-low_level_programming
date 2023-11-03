dfeee6b890a6a4c2fcee96d7bdec32d5b70dd664
Simon tanyi <simontanyi2004@gmail.com>
2023-11-03 02:34:31 -0700

    updates if file content is null

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (text_content == NULL)
 	{
-		return (-1);
+		fd = open(filename, O_WRONLY | O_CREAT);
 	}
 	if (fd == -1)
 		return (-1);
