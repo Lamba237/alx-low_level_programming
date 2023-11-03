cd37a96488ef490326fbb1db74c1a0781c4bbf43
Simon tanyi <simontanyi2004@gmail.com>
2023-11-03 02:16:27 -0700

    updates

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (text_content == NULL)
 	{
 		return (-1);
@@ -31,10 +31,6 @@ int create_file(const char *filename, char *text_content)
 	{
 		close(fd);
 		return (-1);
-	}
-	if (access(filename, F_OK) == 0)
-	{
-		fd = open(filename, O_WRONLY | O_TRUNC);
 	}
 	close(fd);
 	return (1);
