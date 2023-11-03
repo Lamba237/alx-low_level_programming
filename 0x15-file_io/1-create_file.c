2791a6ff712595a4ba63b0d3ced1209500f3541d
Simon tanyi <simontanyi2004@gmail.com>
2023-11-03 02:40:06 -0700

    updates

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,11 +22,12 @@ int create_file(const char *filename, char *text_content)
 	{
 		fd = open(filename, O_TRUNC);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT);
+		fd = open(filename, O_CREAT);
 	}
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+
 	if (fd == -1)
 		return (-1);
 	/* writing into the file using */
