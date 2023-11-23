66e0835b897d7ba31104eac3fc1a983f35d5c7c3
Lamba237 <simontanyi2004@gmail.com>
2023-11-23 08:10:49 -0800

    task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to be converted to binary
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned holder = 0;
+
+	/*unsigned long int divider = 1;*/
+
+		if (n > 1)
+		{
+			print_binary(n >> 1);
+		}
+		holder = (n & 1);
+		_putchar((holder) + '0');
+}
