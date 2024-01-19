24e6ab781d2aec98d4369a6638810486aeef71c3
Lamba237 <simontanyi2004@gmail.com>
2024-01-19 14:22:21 -0800

    updates

---
+++ 0x18-dynamic_libraries/_maths.c
@@ -0,0 +1,51 @@
+#include "operations.h"
+/**
+ * add - adds two variables
+ * @a: var 1
+ * @b: var 2
+ * Return: a+b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtract two variables
+ * @a: var 1
+ * @b: var 2
+ * Return: a-b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - compute product
+ * @a: var 1
+ * @b: var 2
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - function to divide two numbers
+ * @a: var 1
+ * @b: var 2
+ * Return: a/b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modulus
+ * @a: va1 1
+ * @b: var 2
+ * Return: a%b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
